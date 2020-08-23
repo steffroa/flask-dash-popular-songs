4d8416a413a67f884ff10a58bfd97bd32404f8e5
Stefania <steffyroa@gmail.com>
2020-08-22 21:50:09 -0500

    dash-app Add parameter to dash creation app

--- dash-app.py
+++ dash-app.py
@@ -7,7 +7,7 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 df = pd.read_csv(os.environ['POPULAR_SONGS_DATA'])
 
