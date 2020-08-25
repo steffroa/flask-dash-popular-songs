da44af321352094b1e24d59f1fd6f335df4851d0
Stefania <steffyroa@gmail.com>
2020-08-25 11:05:40 -0500

    Fix name from dash and flask app in create_app function

--- popular_songs/__init__.py
+++ popular_songs/__init__.py
@@ -4,14 +4,14 @@ from flask import Flask
 
 def create_app():
     """Construct core Flask application."""
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    flask_app = Flask(__name__, instance_relative_config=False)
+    flask_app.config.from_object('config.Config')
 
-    with app.app_context():
+    with flask_app.app_context():
         # Import Flask routes
         from .interface import controller
 
         from .interface.dashboard import create_dashboard
-        app = create_dashboard(app)
+        flask_dash_app = create_dashboard(flask_app)
 
-        return app
+        return flask_dash_app
